b4477d9fbc16c644e597801e6870eec57e9beed9
NicolasCaous <caousn@hotmail.com>
2017-09-24 22:43:42 -0300

    Record and replay test

--- src/main.cpp
+++ src/main.cpp
@@ -6,57 +6,101 @@
 #include "../include/exception"
 #include "../include/boost/chrono.hpp"
 #include "../include/boost/thread/thread.hpp"
+#include "../include/X11/Xlib.h"
 #include "./Utils/Timer.cpp"
+#include "./Utils/XY.h"
 #include "MouseTracker.h"
 #include "ThreadController/ThreadController.h"
 
-void funcao(std::vector< void* >& params)
+std::vector< XY > track;
+
+void gravar(std::vector< void* >& params)
 {
     Timer* timer = (Timer*) params[params.size() - 1];
+    MouseTracker* mt1 = MouseTracker::get_instance();
+    XY anterior;
     while (true)
     {
         boost::this_thread::sleep_for(boost::chrono::milliseconds(*((int*) params[0]) - timer->elapsed()));
         timer->reset();
-        MouseTracker* mt1 = MouseTracker::get_instance();
-        MouseTracker* mt2 = MouseTracker::get_instance();
+        XY now;
+            now.x = mt1->get_coordinates().x;
+            now.y = mt1->get_coordinates().y;
+        /*if (track.size() == 0)
+        {
+            anterior.x = now.x;
+            anterior.y = now.y;
 
-        std::cout << ThreadController::active() << std::endl;
-        std::cout << mt1->get_coordinates().x << std::endl;
-        std::cout << mt2->get_coordinates().y << std::endl;
+            track.push_back(anterior);
+        }
+        else
+        {
+            anterior.x = now.x - anterior.x;
+            anterior.y = now.y - anterior.y;
+
+            track.push_back(anterior);
+
+            anterior.x = now.x;
+            anterior.y = now.y;
+        }*/ track.push_back(now);
     }
+}
+
+void reproduzir(std::vector< void* >& params)
+{
+    Timer* timer = (Timer*) params[params.size() - 1];
+    int i = 0;
+    Display *d = XOpenDisplay(0);
+    Window root_window;
+    root_window = XRootWindow(d, 0);
+
+    MouseTracker* mt = MouseTracker::get_instance();
+
+    /*XWarpPointer(
+                d, None, root_window,
+                0, 0, 0, 0,
+                -1920, -1920
+            );
+    XFlush(d);*/
+
+    while (true)
+    {
+        boost::this_thread::sleep_for(boost::chrono::milliseconds(*((int*) params[0]) - timer->elapsed()));
+        timer->reset();
+
+        if (d) {
+            //XY cood = mt->get_coordinates();
+            XWarpPointer(
+                d, None, root_window,
+                0, 0, 0, 0,
+                track[i].x, track[i].y
+            );
+            XFlush(d);
+        }
+        ++i;
+    }
+
+    XCloseDisplay(d);
+
 }
 
 int main(int argc, char ** argv) {
 
     std::vector< void* > params;
-    int x = 200;
+    int x = 20;
     params.push_back(&x);
     ThreadController* tc = ThreadController::get_instance();
 
-    tc->create_thread("1", "num", &funcao, params);
-    sleep(2);
+    sleep(1);
+    tc->create_thread("1", "num", &gravar, params);
+    sleep(5);
+    tc->close_thread("1");
+    tc->create_thread("2", "num", &reproduzir, params);
+    sleep(5);
+    tc->close_thread("2");
+    sleep(5);
     tc->close_all_threads();
     std::cout << ((Timer*) params[params.size()-1])->elapsed() << std::endl;
-
-    /*Display *d = XOpenDisplay(0);
-
-    if (d) {
-        Window root_window;
-        root_window = XRootWindow(d, 0);
-        XWarpPointer(
-            d, None, root_window,
-            0, 0, 0, 0,
-            -1920, -1920
-        );
-        XFlush(d);
-        sleep(1);
-        XWarpPointer(
-            d, None, root_window,
-            0, 0, 0, 0,
-            1920, 1920
-        );
-        XFlush(d);
-    }*/
 
     return 0;
 }
